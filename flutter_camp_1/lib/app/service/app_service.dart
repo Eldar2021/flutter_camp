923fa0c676fb607768ccce02dd005d90fa50390f
Janara Sourbayeva <janara2610@gmail.com>
2023-03-11 20:57:34 +0600

    added AppConsts and App Service

--- flutter_camp_1/lib/app/service/app_service.dart
+++ flutter_camp_1/lib/app/service/app_service.dart
@@ -1,1 +1,14 @@
-class AppService {}
+import 'package:flutter/material.dart';
+import 'package:flutter_camp_1/constants/app_constants/app_const.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppService {
+  final SharedPreferences preferences;
+
+  const AppService(this.preferences);
+
+  Locale init() {
+    final langCode = preferences.getString(AppConsts.languageCodeKey);
+    return Locale(langCode ?? 'en');
+  }
+}
---
+++ flutter_camp_1/lib/constants/constants.dart
@@ -0,0 +1,1 @@
+export 'app_constants/app_const.dart';
